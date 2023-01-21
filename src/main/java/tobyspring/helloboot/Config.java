5b03ea912016c829b0a8a61c287abcb5dbca4305
CHOIJIHO <wlghsp@gmail.com>
2023-01-21 22:32:24 +0900

    합성 애노테이션의 적용

--- src/main/java/tobyspring/helloboot/Config.java
+++ src/main/java/tobyspring/helloboot/Config.java
@@ -1,17 +1,20 @@
 package tobyspring.helloboot;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.DispatcherServlet;
 
-@MySpringBootAnnotation
-public class HellobootApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(HellobootApplication.class, args);
+@Configuration
+public class Config {
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory() {
+        return new TomcatServletWebServerFactory();
     }
-
+    @Bean
+    public DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
+    }
 }

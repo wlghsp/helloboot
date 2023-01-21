b545c9626f1bce17caa367f6f2ba1c27a026a8ef
CHOIJIHO <wlghsp@gmail.com>
2023-01-21 16:33:14 +0900

    테스트 코드를 이용한 테스트

---
+++ src/test/java/tobyspring/helloboot/HelloApiTest.java
@@ -0,0 +1,27 @@
+package tobyspring.helloboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class HelloApiTest {
+    @Test
+    void helloApi() {
+        TestRestTemplate rest = new TestRestTemplate();
+
+        ResponseEntity<String> res =
+                rest.getForEntity("http://localhost:8080/hello?name={name}", String.class, "Spring");
+
+        // status code 200
+        assertThat(res.getStatusCode()).isEqualTo(HttpStatus.OK);
+        // header(content-type) text/plain
+        assertThat(res.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).startsWith(MediaType.TEXT_PLAIN_VALUE);
+        // body Bye Spring
+        assertThat(res.getBody()).isEqualTo("Bye Spring");
+    }
+}
